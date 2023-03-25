08bb95a3150619e008e55a0d1d87101cf0ae85e3
siva <ahnashwin1305@gmail.com>
2023-03-25 19:28:20 +0530

    user auth added

--- swiftaid_backend/settings.py
+++ swiftaid_backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'api',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -111,6 +113,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 
 # Static files (CSS, JavaScript, Images)
---
+++ users/views.py
@@ -0,0 +1,40 @@
+import requests
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+from django.urls import reverse
+import time
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+
+
+def register(request):
+    # if request.user.is_authenticated:
+    #     return redirect(reverse('home'))
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            messages.success(request, 'Your account has been created! You are now able to log in')
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password1'],
+                                )
+            login(request, new_user)
+            return redirect('profile')
+ 
+
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+
+@login_required
+def logout(request):
+    time.sleep(2)
+    logout(request)
+    messages.success(request, 'You are logged out successfully')
+    return redirect('login')
