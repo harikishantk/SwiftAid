1e645318f1484b19de1c34b5db9025c0890515e1
siva <ahnashwin1305@gmail.com>
2023-03-25 20:25:35 +0530

    added options for registration

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+CHOICES = [
+    ('VICTIM','VICTIM'),
+    ('VOLUNTEER','VOLUNTEER')
+]
 
 class UserRegisterForm(UserCreationForm):
-	email = forms.EmailField()
+    email = forms.EmailField()
+    option = forms.CharField(label="Register as: ", widget=forms.Select(choices=CHOICES))
 
-	class Meta:
-		model = User 
-		fields = ['username','email','password1','password2']
+    class Meta:
+        model = User 
+        fields = ['username', 'option', 'email','password1','password2']
+
+
+class LoginForm(AuthenticationForm):
+    email = forms.EmailField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
+    option = forms.CharField(label="Login as: ", widget=forms.Select(choices=CHOICES))
+
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'option')
+        
